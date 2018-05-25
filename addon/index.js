1946b56945e2fc413311a5d782f7748fab0d500c
John Cowen <jcowen@hashicorp.com>
2018-05-25 14:20:43 +0100

    Initial import

---
+++ addon/index.js
@@ -0,0 +1,70 @@
+import Pretender from 'pretender';
+import apiFactory from '@johncowen/api-double';
+export default function(path, setCookies, typeToURL) {
+  const createAPI = apiFactory(12345, path);
+  let api = createAPI();
+  let cookies = {};
+  let history = [];
+  const server = new Pretender();
+  server.handleRequest = request => {
+    let url = request.url.split('?')[0];
+    history.push(request);
+    const req = {
+      path: url,
+      url: url,
+      query: request.queryParams || {},
+      headers: request.requestHeaders,
+      body: request.requestBody,
+      method: request.method,
+      cookies: cookies,
+    };
+    const response = {
+      set: function() {
+        // console.log('setting');
+      },
+      send: function(response) {
+        request.respond(200, { 'Content-Type': 'application/json' }, response);
+      },
+    };
+    api.serve(req, response, function() {});
+  };
+  return {
+    server: {
+      history: history,
+      reset: function() {
+        api = createAPI();
+        cookies = {};
+        history = [];
+        this.history = history;
+      },
+      createList: function(type, num, value) {
+        const url = typeToURL(type);
+        cookies = setCookies(type, num, cookies);
+        if (url && value) {
+          api.mutate(function(response, config) {
+            return response.map((item, i, arr) => {
+              let res = value;
+              if (typeof value === 'object') {
+                if (value.constructor == Object) {
+                  // res = { ...item, ...value };
+                  res = Object.assign(item, value);
+                } else if (value.constructor == Array) {
+                  // res = { ...item, ...value[i] };
+                  if(value[i]) {
+                    if(typeof value[i] === "object") {
+                      res = Object.assign(item, value[i]);
+                    } else {
+                      res = value[i];
+                    }
+                  }
+                }
+              }
+              return res;
+            });
+          }, url);
+        }
+      },
+    },
+  };
+
+}
---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = {
+  name: '@johncowen/ember-cli-api-double',
+  treeFor: function() {
+    let app;
+
+    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+    // use that.
+    if (typeof this._findHost === 'function') {
+      app = this._findHost();
+    } else {
+      // Otherwise, we'll use this implementation borrowed from the _findHost()
+      // method in ember-cli.
+      let current = this;
+      do {
+        app = current.app || app;
+      } while (current.parent.parent && (current = current.parent));
+    }
+
+    this.app = app;
+    if (app.env !== 'test') {
+      return;
+    }
+    return this._super.treeFor.apply(this, arguments);
+  },
+  included: function() {
+    let app;
+
+    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+    // use that.
+    if (typeof this._findHost === 'function') {
+      app = this._findHost();
+    } else {
+      // Otherwise, we'll use this implementation borrowed from the _findHost()
+      // method in ember-cli.
+      let current = this;
+      do {
+        app = current.app || app;
+      } while (current.parent.parent && (current = current.parent));
+    }
+
+    this.app = app;
+    // this.addonConfig = this.app.project.config(app.env)['ember-cli-mirage'] || {};
+    // this.addonBuildConfig = this.app.options['ember-cli-mirage'] || {};
+
+    // Call super after initializing config so we can use _shouldIncludeFiles for the node assets
+    this._super.included.apply(this, arguments);
+
+    if (app.env === 'test') {
+      app.import('node_modules/@johncowen/api-double/index.js', {
+        using: [
+          { transformation: 'cjs', as: '@johncowen/api-double' }
+        ]
+      });
+      app.import('node_modules/array-range/index.js', {
+        using: [
+          { transformation: 'cjs', as: 'array-range' }
+        ]
+      });
+      app.import('node_modules/pretender/pretender.js', {
+        using: [
+          { transformation: 'cjs', as: 'pretender' }
+        ]
+      });
+      app.import('node_modules/js-yaml/index.js', {
+        using: [
+          { transformation: 'cjs', as: 'js-yaml' }
+        ]
+      });
+    }
+  },
+};
