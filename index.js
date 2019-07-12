232e912fda6f68924f48fa05c487099aaf50ee4e
John Cowen <jcowen@hashicorp.com>
2019-07-12 18:19:39 +0000

    Shim in an instance-initializer if required

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
 
+const path = require('path');
+const fs = require('fs');
+const mergeTrees = require('broccoli-merge-trees');
+const Funnel = require('broccoli-funnel');
+const writeFile = require('broccoli-file-creator');
+
 const readdir = require('recursive-readdir-sync');
 const read = require('fs').readFileSync;
+
 module.exports = {
   name: require('./package.json').name,
   contentFor: function(type, config) {
@@ -26,7 +33,21 @@ module.exports = {
       }
     }
   },
-  treeFor: function() {
+  treeForApp: function(appTree) {
+    const config = this.app.project.config(this.app.env) || {};
+    const addon = config[this.name] || {enabled: false};
+    if (!addon.enabled) {
+      return;
+    }
+    const dir = 'api-double';
+    const trees = [appTree];
+    const addonTree = new Funnel(path.join(this.app.project.root, `/${dir}`), {
+      destDir: dir
+    });
+    trees.push(addonTree);
+    return mergeTrees(trees);
+  },
+  treeFor: function(name) {
     let app;
 
     // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
@@ -48,6 +69,23 @@ module.exports = {
 
     if (!addon.enabled) {
       return;
+    }
+    if(addon['auto-import'] !== false && name === 'app') {
+      const dir = 'api-double';
+      if(fs.existsSync(path.join(this.app.project.root, `/${dir}/index.js`))) {
+        const tree = writeFile('instance-initializers/ember-cli-api-double.js', `
+            import api from '${config.modulePrefix}/${dir}';
+            export default {
+              name: 'ember-cli-api-double',
+              initialize: function() {}
+            };
+        `);
+        return mergeTrees([
+          tree,
+          this._super.treeFor.apply(this, arguments)
+        ]);
+      }
+
     }
     return this._super.treeFor.apply(this, arguments);
   },
